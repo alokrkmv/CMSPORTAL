bc0bd5cacbe456bbf2682164686dc6b97be5f857
alokrkmv <alokrkmv12@gmail.com>
2018-02-01 01:08:36 +0530

    Made a simple layout for Dashboard

---
+++ dashboard.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/adminstyles.css">
+
+
+    </script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-2">
+          <h1>ALOK</h1>
+          <ul id="side_menu" class="nav nav-pills nav-stacked">
+            <li class="active"><a href="Dashboard.php">Dashboard</a></li>
+            <li><a href="#">Add New Post</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">Add New Post</a></li>
+            <li><a href="#">Manage Admins</a></li>
+            <li><a href="#">Comments</a></li>
+            <li><a href="#">Live Blog</a></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-10">
+          <h1>About</h1>
+          <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+            language I like this langauge very much because this the best language I
+            ever had and this gives me to create more and more things</p>
+          <h1>About</h1>
+          <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+            language I like this langauge very much because this the best language I
+            ever had and this gives me to create more and more things</p>
+          <h1>About</h1>
+          <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+            language I like this langauge very much because this the best language I
+            ever had and this gives me to create more and more things</p>
+          <h1>About</h1>
+          <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+            language I like this langauge very much because this the best language I
+            ever had and this gives me to create more and more things</p>
+          <h1>About</h1>
+          <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+            language I like this langauge very much because this the best language I
+            ever had and this gives me to create more and more things</p>
+            <h1>About</h1>
+            <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+              language I like this langauge very much because this the best language I
+              ever had and this gives me to create more and more things</p>
+              <h1>About</h1>
+              <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+                language I like this langauge very much because this the best language I
+                ever had and this gives me to create more and more things</p>
+                <h1>About</h1>
+                <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+                  language I like this langauge very much because this the best language I
+                  ever had and this gives me to create more and more things</p>
+                  <h1>About</h1>
+                  <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+                    language I like this langauge very much because this the best language I
+                    ever had and this gives me to create more and more things</p>
+                    <h1>About</h1>
+                    <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+                      language I like this langauge very much because this the best language I
+                      ever had and this gives me to create more and more things</p>
+                      <h1>About</h1>
+                      <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+                        language I like this langauge very much because this the best language I
+                        ever had and this gives me to create more and more things</p>
+                        <h1>About</h1>
+                        <p>My favourite langauge is <em>c++</em>and it is a  very nice and great
+                          language I like this langauge very much because this the best language I
+                          ever had and this gives me to create more and more things</p>
+        </div>
+      </div>
+    </div>
+
+
+
+  </body>
+</html>
